64d61e88bfbee82017315e5df233ad9f3c8680f7
meankit2610 <ankitsingh5282770@gmail.com>
2022-12-01 12:37:24 +0530

    signup,upload, videos page complete

--- game-chakra-ui/src/App.js
+++ game-chakra-ui/src/App.js
@@ -2,6 +2,9 @@ import {BrowserRouter as Router , Routes, Route} from "react-router-dom"
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Upload from "./components/Upload";
+import Videos from "./components/Videos";
 
 function App() {
   return (
@@ -9,7 +12,10 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<Videos />} />
         <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/upload" element={<Upload/>}/>
       </Routes>
   </Router>
   );
